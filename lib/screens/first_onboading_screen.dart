1c400bb4c57b4f50bba683269bb33e3d4112494c
arunaguragain <guragainaruna@gmail.com>
2025-12-04 11:54:12 +0545

    Splash Screen Completed

---
+++ lib/screens/first_onboading_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstOnboadingScreen extends StatelessWidget {
+  const FirstOnboadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("On boarding"),),
+      body: Column(),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:aashwaas/screens/first_onboading_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+          builder: (context) => const FirstOnboadingScreen(),
+        ),
+      );
+    });
+
+    final _gap = SizedBox(height: 25);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue.shade50,
+              ),
+              child: Image.asset("assets/images/logo.png"),
+            ),
+
+            _gap,
+
+            Text(
+              "Aashwaas",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+
+            _gap,
+
+            Container(
+              width: 100,
+              height: 5,
+              color: Colors.blueAccent,
+              child: LinearProgressIndicator(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
