91e69caaffc81d0dbe96392bf34886c714a2c12f
arunaguragain <guragainaruna@gmail.com>
2025-12-04 21:58:21 +0545

    Login as Donor Screen Completed

--- lib/screens/donor_login_screen.dart
+++ lib/screens/donor_login_screen.dart
@@ -1,4 +1,7 @@
+import 'package:aashwaas/widgets/my_button.dart';
+import 'package:aashwaas/widgets/my_textformfield.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DonorLoginScreen extends StatefulWidget {
   const DonorLoginScreen({super.key});
@@ -8,8 +11,124 @@ class DonorLoginScreen extends StatefulWidget {
 }
 
 class _DonorLoginScreenState extends State<DonorLoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _gap = SizedBox(height: 15);
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 211, 227, 255),
+      appBar: AppBar(backgroundColor: const Color.fromARGB(255, 211, 227, 255)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Container(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.asset("assets/images/logo.png"),
+                ),
+
+                Text(
+                  " Login as Donor",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+
+                _gap,
+
+                MyTextformfield(
+                  hintText: "Enter your email",
+                  controller: _emailController,
+                  labelText: "Your email",
+                  errorMessage: "Email is required",
+                ),
+
+                _gap,
+
+                MyTextformfield(
+                  hintText: "Enter your password",
+                  controller: _passwordController,
+                  labelText: "Password",
+                  errorMessage: "Password is required",
+                ),
+
+                _gap,
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Forget Password?",),
+                    ),
+                  ],
+                ),
+
+                _gap,
+
+                MyButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  text: "Login",
+                  color: Colors.blueAccent,
+                ),
+
+                SizedBox(height: 7),
+
+                Row(
+                  children: [
+                    Expanded(child: Divider()),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Or"),
+                    ),
+                    Expanded(child: Divider()),
+                  ],
+                ),
+
+                SizedBox(height: 7),
+
+                MyButton(
+                  onPressed: () {},
+                  text: "Login with Google",
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  icon: Icon(FontAwesomeIcons.google),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    TextButton(onPressed: () {}, child: Text("Sign Up")),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Login as Volunteer"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/my_button.dart
+++ lib/widgets/my_button.dart
@@ -1,3 +1,42 @@
+// import 'package:flutter/material.dart';
+
+// class MyButton extends StatelessWidget {
+//   const MyButton({
+//     super.key,
+//     required this.onPressed,
+//     required this.text,
+//     this.color,
+//     this.textColor, 
+//     this.icon,
+//   });
+
+//   //on pressed Callback
+//   final VoidCallback onPressed;
+//   final String text;
+//   final Color? color;
+//   final Color? textColor;
+//   final Icon? icon;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       width: 500,
+//       child: ElevatedButton(
+//         style: ElevatedButton.styleFrom(
+//           backgroundColor: color ?? Colors.blueAccent,
+//           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+//         ),
+
+//         onPressed: onPressed,
+//         child: Text(
+//           text,
+//           style: TextStyle(color: textColor ?? Colors.white, fontSize: 20),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
@@ -6,14 +45,15 @@ class MyButton extends StatelessWidget {
     required this.onPressed,
     required this.text,
     this.color,
-    this.textColor,
+    this.textColor, 
+    this.icon,
   });
 
-  //on pressed Callback
   final VoidCallback onPressed;
   final String text;
   final Color? color;
   final Color? textColor;
+  final Icon? icon;
 
   @override
   Widget build(BuildContext context) {
@@ -26,10 +66,26 @@ class MyButton extends StatelessWidget {
         ),
 
         onPressed: onPressed,
-        child: Text(
-          text,
-          style: TextStyle(color: textColor ?? Colors.white, fontSize: 20),
-        ),
+        
+        child: icon == null
+            ? Text(
+                text,
+                style: TextStyle(color: textColor ?? Colors.white, fontSize: 20),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  icon!,                       // ← your icon
+                  const SizedBox(width: 10),   // space between icon and text
+                  Text(
+                    text,
+                    style: TextStyle(
+                      color: textColor ?? Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
