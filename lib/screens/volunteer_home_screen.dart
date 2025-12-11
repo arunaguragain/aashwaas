ead8034e55e1dd6ce08a93ed9ba32abb1b151fd4
arunaguragain <guragainaruna@gmail.com>
2025-12-11 17:37:01 +0545

    added the Bottom Navigation Bar in volunteer home screen

--- lib/screens/volunteer_home_screen.dart
+++ lib/screens/volunteer_home_screen.dart
@@ -1,13 +1,49 @@
+import 'package:aashwaas/screens/bottom_screen_donor/history_screen.dart';
+import 'package:aashwaas/screens/bottom_screen_volunteer/home_screen.dart';
+import 'package:aashwaas/screens/bottom_screen_volunteer/profile_screen.dart';
+import 'package:aashwaas/screens/bottom_screen_volunteer/task_screen.dart';
 import 'package:flutter/material.dart';
 
-class VolunteerHomeScreen extends StatelessWidget {
+class VolunteerHomeScreen extends StatefulWidget {
   const VolunteerHomeScreen({super.key});
+
+  @override
+  State<VolunteerHomeScreen> createState() => _VolunteerHomeScreenState();
+}
+
+class _VolunteerHomeScreenState extends State<VolunteerHomeScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const TaskScreen(),
+    const HistoryScreen(),
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("I am Volunteer Home Screen")),
-      body: Container(),
+      appBar: AppBar(),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.task), label: 'Task'),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
