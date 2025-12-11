bad2283893fa1d90f820735c87b2775ec7246a1a
arunaguragain <guragainaruna@gmail.com>
2025-12-11 17:19:48 +0545

    Added the bottom screens of Volunteer

---
+++ lib/screens/bottom_screen_volunteer/task_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(child: Text('Welcome to Task Screen')),
+    );
+  }
+}
